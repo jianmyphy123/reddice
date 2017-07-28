3dfc3f88a4755889576af13a704fb9f57c011b72
jianmyphy123 <jianmyphy123@tutanota.com>
2017-07-28 06:10:34 +0200

    Adds client-side validation, Extracts TextFieldGroup

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -4,6 +4,8 @@ import timezones from '../../data/timezones';
 import map from 'lodash/map';
 import axios from 'axios';
 import classnames from 'classnames';
+import validateInput from '../../../server/shared/validations/signup';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -29,12 +31,26 @@ class SignupForm extends React.Component {
     });
   }
 
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+
+    if(!isValid) {
+      this.setState({ errors });
+    }
+
+    return isValid;
+  }
+
   onSubmit(e) {
-    this.setState({errors: {}, isLoading: true});
+
     e.preventDefault();
-    this.props.userSignupRequest(this.state).then(
-      ({data}) => this.setState({errors: data, isLoading: false})
-    );
+
+    if(this.isValid()) {
+      this.setState({errors: {}, isLoading: true});
+      this.props.userSignupRequest(this.state).then(
+        ({data}) => this.setState({errors: data, isLoading: false})
+      );
+    }
   }
 
   render() {
@@ -48,26 +64,36 @@ class SignupForm extends React.Component {
     return (
       <form onSubmit={this.onSubmit}>
         <h1>Join our community!</h1>
-        <div className={classnames('form-group', {'has-error': errors.username})}>
-          <label className="control-label">Username</label>
-          <input value={this.state.username} onChange={this.onChange} type="text" name="username" className="form-control"/>
-          {errors.username && <span className="help-block">{errors.username}</span>}
-        </div>
-        <div className={classnames('form-group', {'has-error': errors.email})}>
-          <label className="control-label">Email</label>
-          <input value={this.state.email} onChange={this.onChange} type="text" name="email" className="form-control"/>
-          {errors.email && <span className="help-block">{errors.email}</span>}
-        </div>
-        <div className={classnames('form-group', {'has-error': errors.password})}>
-          <label className="control-label">Password</label>
-          <input value={this.state.password} onChange={this.onChange} type="password" name="password" className="form-control"/>
-          {errors.password && <span className="help-block">{errors.password}</span>}
-        </div>
-        <div className={classnames('form-group', {'has-error': errors.passwordConfirmation})}>
-          <label className="control-label">PasswordConfirmation</label>
-          <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password" name="passwordConfirmation" className="form-control"/>
-          {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
-        </div>
+          <TextFieldGroup
+            error={errors.username}
+            label="Username"
+            onChange={this.onChange}
+            value={this.state.username}
+            field="username"
+          />
+
+          <TextFieldGroup
+            error={errors.email}
+            label="Email"
+            onChange={this.onChange}
+            value={this.state.email}
+            field="email"
+          />
+
+          <TextFieldGroup
+            error={errors.password}
+            label="Password"
+            onChange={this.onChange}
+            value={this.state.password}
+            field="password"
+          />
+          <TextFieldGroup
+            error={errors.passwordConfirmation}
+            label="PasswordConfirmation"
+            onChange={this.onChange}
+            value={this.state.passwordConfirmation}
+            field="passwordConfirmation"
+          />
         <div className={classnames('form-group', {'has-error': errors.timezone})}>
           <label className="control-label">Timezone</label>
           <select value={this.state.timezone} onChange={this.onChange} name="timezone" className="form-control">
