e36fe5a4ca751d8e840b44835e4da4c76b093dee
jianmyphy123 <jianmyphy123@tutanota.com>
2017-07-30 08:39:55 +0200

    Login and logout on client

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,24 +1,58 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../actions/authActions';
 
+class NavigationBar extends React.Component {
 
-export default () => {
-  return (
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <NavLink className="navbar-brand" to="/">Red Dice</NavLink>
+  logout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  render() {
+
+    const { isAuthenticated, user } = this.props.auth;
+
+    const userLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href='#' onClick={this.logout.bind(this)}>Logout</a></li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><NavLink activeClassName="active" to="/signup">Sign Up</NavLink></li>
+        <li><NavLink activeClassName="active" to="/login">Login</NavLink></li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <NavLink className="navbar-brand" to="/">Red Dice</NavLink>
+          </div>
+          <div className="collapse navbar-collapse">
+            { isAuthenticated ? userLinks : guestLinks }
+          </div>
         </div>
-        <div className="collapse navbar-collapse">
+      </nav>
+    );
+  }
 
-            <ul className="nav navbar-nav navbar-right">
-              <li><NavLink activeClassName="active" to="/signup">Sign Up</NavLink></li>
-              <li><NavLink activeClassName="active" to="/login">Login</NavLink></li>
-            </ul>
-        </div>
-      </div>
-    </nav>
-
-
-  );
 }
+
+NavigationBar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
--- client/utils/setAuthorizationToken.js
+++ client/utils/setAuthorizationToken.js
@@ -4,6 +4,6 @@ export default function setAuthorizationToken(token) {
   if(token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
-    delete axios.defaults.headers.commont['Authorization'];
+    delete axios.defaults.headers.common['Authorization'];
   }
 }
